f9b148aa560b54f45050353c627507096df807a9
Mario Ramírez <marioh.ramirez@udea.edu.co>
2023-09-26 14:58:13 -0500

    Configuración a seguimiento de imagen
    Se ha realizado un script que permite saber cuando el marcador ha sido identificado para iniciar la reproducción de un vídeo

---
+++ Assets/Scripts/ImageTrackerManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ImageTrackerManager : MonoBehaviour
+{
+    [SerializeField] private ARTrackedImageManager aRTrackedImageManager;
+    private VideoPlayer videoPlayer;
+    //El siguiente booleano nos ayuda a saber cuando la imagen puede o no ser rastreada
+    private bool isImageTrackable;
+
+    private void OnEnebale()
+    {
+        aRTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnDisable()
+    {
+        aRTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    //Funcion que permite que el video se reproduzca solo cuando la imagen esta siendo captada
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs eventData)
+    {
+        foreach(var trackedImage in eventData.added)
+        {
+            videoPlayer = trackedImage.GetComponentInChildren<VideoPlayer>();
+            videoPlayer.Play();
+        }
+
+        foreach(var trackedImage in eventData.updated)
+        {
+            if(trackedImage.trackingState == TrackingState.Tracking)
+            {
+                if(!isImageTrackable)
+                {
+                    isImageTrackable = true;
+                    videoPlayer.gameObject.SetActive(true);
+                    videoPlayer.Play();
+                }
+            }
+            else if(trackedImage.trackingState == TrackingState.Limited)
+            {
+                if(!isImageTrackable)
+                {
+                    isImageTrackable = false;
+                    videoPlayer.gameObject.SetActive(true);
+                    videoPlayer.Pause();
+                }
+            }
+        }
+    }
+
+}
